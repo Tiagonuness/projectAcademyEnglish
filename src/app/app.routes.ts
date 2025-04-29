662689529ef852655d8c49e197041108d706cb76
Tiago nunes <117403385+Tiagonuness@users.noreply.github.com>
2025-04-29 15:05:49 -0300

    home create

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
     {
         path: "login",
-        component: LoginComponent        
+        component: LoginComponent
+    },
+    {
+        path: "home",
+        component: HomeComponent
     }
 ];
